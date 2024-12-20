7d5ffe2fc2e76514f906d8f3f01fb93de024a19f
leonardo lobo <loboleonardo75@gmail.com>
2024-12-19 23:45:24 -0400

    Commit

---
+++ Mecanografia-Juego/Models/Datos.cs
@@ -0,0 +1,31 @@
+namespace Datos
+{
+    public class Datos
+    {
+        public static string[] palabras = {
+            "quimica", "realidad", "sabiduria", "sueno", "talento", "universo", "valentia", "viento", "vision", "vitalidad",
+            "hola","mundo","programacion","csharp","visual","studio","teclado","raton","monitor","pantalla","computadora",
+            "laptop","celular","telefono","internet","conexion","inalambrico","inalambrica","inalambricos","inalambricas","inalambricamente",
+            "abaco", "abandonar", "abierto", "abogado", "abrumar", "abundante", "acelerar", "acertar", "aclarar", "acompanar",
+            "acordar", "acostumbrar", "actitud", "adaptar", "admirar", "adquirir", "afirmar", "agregar", "agrupar", "alabar",
+            "aliviar", "alquilar", "ampliar", "analizar", "anunciar", "aprender", "apreciar", "arreglar", "asumir", "atender",
+            "atrapar", "aumentar", "avanzar", "bailar", "bajar", "balancear", "barrer", "buscar", "cambiar", "caminar",
+            "cantar", "captar", "celebrar", "cercano", "citar", "clarificar", "coincidir", "comenzar", "comprar", "conectar",
+            "construir", "contar", "conversar", "correr", "crear", "decidir", "defender", "dejar", "demostrar", "desarrollar",
+            "descubrir", "desechar", "disenar", "disfrutar", "dividir", "educar", "elegir", "empezar", "encontrar", "enviar",
+            "escribir", "escuchar", "esperar", "estudiar", "evaluar", "examinar", "explicar", "explorar", "festejar", "fijar",
+            "formar", "fortalecer", "fumar", "ganar", "gastar", "gritar", "hablar", "hacer", "imprimir", "incluir",
+            "informar", "iniciar", "inspirar", "intentar", "invitar", "jugar", "juntar", "luchar", "mirar", "modificar",
+            "mover", "navegar", "necesitar", "observar", "ocupar", "ofrecer", "organizar", "pagar", "participar", "pensar",
+            "perder", "planificar", "platicar", "preguntar", "preparar", "probar", "producir", "prometer", "proteger", "publicar",
+            "quedar", "realizar", "recibir", "reconocer", "reducir", "reflejar", "regresar", "relacionar", "repetir", "resolver",
+            "revisar", "saber", "sacar", "saludar", "salvar", "seguir", "seleccionar", "sentir", "servir", "sugerir",
+            "suspender", "tocar", "trabajar", "traducir", "transformar", "transmitir", "unir", "usar", "valorar", "vender",
+            "ver", "viajar", "vivir", "volar", "volver", "zambullir", "zancada", "zapato", "zoologico", "zumbido",
+            "abundancia", "acertijo", "acuario", "adversidad", "afecto", "agricultura", "alimento", "alquimia", "analisis", "aprecio",
+            "aroma", "asombro", "atencion", "belleza", "busqueda", "ciencia", "cielo", "cultura", "destino", "dibujo",
+            "educacion", "esencia", "esperanza", "futuro", "galaxia", "horizonte", "ilusion", "infinito", "inspiracion", "jardin",
+            "juventud", "libertad", "luz", "magia", "maravilla", "naturaleza", "oportunidad", "pasion", "pensamiento ", "paz",
+        };
+    }
+}
